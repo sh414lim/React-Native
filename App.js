fef93b51d5d4900f74ae3c4231b6ccaceb8663df
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-08-31 23:13:51 +0900

    API Debug

--- App.js
+++ App.js
@@ -1,27 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+import {Alert} from "react-native";
+import * as Location from 'expo-location';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = "c98df855229618a9bfa933d0f92f953f";
+
+export default class extends React.Component{
+  state={
+    isLoading:true,
+  };
+
+  getWeather= async(latitude,longitude)=>{
+    const{data}=await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      );
+      console.log(data);
+    };
+
+  getLocation=async ()=>{
+    try{
+      //애플리케이션 사용자의 현재 위치 확인 프로미스 함수
+      // Location.requestForegroundPermissionsAsync 이 작동하지 않는다면 
+      //프로미스를 리턴한다 
+      //만약 사용자가 permission 을 안주면 에러를 발생시킨다.(자바스크립트 에러)
+      await Location.requestForegroundPermissionsAsync();
+      const {coords :{latitude,longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(lat,long);
+      this.setState({
+          isLoading:false,
+        })
+      // Sebd ti API and get Weather
+    }catch(error){
+      //try 안에 에러가 발생시 실행
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  componentDidMount(){
+    this.getLocation();
+  }
+  render(){
+    const {isLoading}=this.state;
+  return isLoading ? <Loading/> : null; 
+
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 
 //저장 후에 즉시 전체 애플리케이션을 reload한다.
 //splash 스크린이 잠깐 보이고 애플리케이셔이 나타난다.
 //이것을 live load라고 한다 (내가 저장하면 자동으로 리프레쉬)
 //새로고침을 수동으로 하고 싶다면 command or ctrl + R
+//Cordova or PhoneGap 이용해서 .. 그 안에 html CSS 를 넣는다.(웹 안에서 작동하는 웹사이트) => 하이브리드, 웹뷰 방식
+//
---
+++ Loading.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+function Loading(){
+    return <View style={styles.container}>
+        <Text style={styles.text}>Getting The fucking weather</Text>
+    </View>
+
+}
+
+const styles=StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent:"flex-end",
+        padding: 20,
+        paddingHorizontal:30,
+        paddingVertical:100,
+        backgroundColor:"#FDF6AA"
+    },
+    text:{
+        color:"#2c2c2c",
+        fontSize: 30
+    }
+
+})
+
+export default Loading;
+
