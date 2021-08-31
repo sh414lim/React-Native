5ded473e06f386950f8cbeb9a25c76db97422517
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-08-31 21:02:09 +0900

    setting

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -19,3 +19,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+//저장 후에 즉시 전체 애플리케이션을 reload한다.
+//splash 스크린이 잠깐 보이고 애플리케이셔이 나타난다.
+//이것을 live load라고 한다 (내가 저장하면 자동으로 리프레쉬)
+//새로고침을 수동으로 하고 싶다면 command or ctrl + R
