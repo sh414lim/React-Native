403f36564396f75ad948653ed87312dac8093791
sh414lim <77113306+sh414lim@users.noreply.github.com>
2021-09-21 16:51:41 +0900

    수정

--- Loading.js
+++ Loading.js
@@ -14,7 +14,6 @@ const styles=StyleSheet.create({
         container:{
         flex: 1,
         justifyContent:"flex-end",
-        padding: 20,
         paddingHorizontal:30,
         paddingVertical:100,
     },
